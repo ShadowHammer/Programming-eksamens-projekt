88c408cf7768cb94dd4c2ab21408ed779c089d9d
ShadowHammer <63354418+ShadowHammer@users.noreply.github.com>
2020-04-08 17:10:59 +0200

    Add files via upload

---
+++ GUI Lærer/src/gui/StartScreen.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+/**
+ *
+ * @author Tobias
+ */
+public class StartScreen extends JFrame {
+    private JPanel panelAfsluttet;
+    private JPanel panelAktive;
+    private JLabel label1;
+    private JButton knap1;
+    private JButton knap2;
+    private JTabbedPane afsluttedeAfleveringer;
+    private JTabbedPane aktiveAfleveringer;
+    
+    
+    
+    
+    public StartScreen(){
+        creatComponents();
+    }
+
+    private void creatComponents() {
+        
+    }
+}
+    
