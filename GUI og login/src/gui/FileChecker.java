611975353635589f33a53b738d84b131cf7e26ed
manporg <manporg@users.noreply.github.com>
2020-05-08 21:36:16 +0200

    Opdateret klasse
    Tilføjet kommentarer

--- GUI og login/src/gui/FileChecker.java
+++ GUI og login/src/gui/FileChecker.java
@@ -1,31 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package eksamensprojekt.fake.ludus;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.Scanner;
 import java.util.ArrayList;
-/**
- *
- * @author Lasse
- */
+
 public class FileChecker {
 
+    //Klassevariabler til at huse nogle variabler der skal bruges
     private String newPattern = "";
     private ArrayList<String> filtypeListe = new ArrayList<>();
     
-    public ArrayList fileSelector2() {
+    //Metode til at lave listen af godkendte filtyper
+    public ArrayList fileSelecter() {
         int counter = 0;
         Scanner sc = new Scanner(System.in);
+        //Ryder listen af filtyper, så den er tom fra starten hver gang
         filtypeListe.clear();
         
         System.out.println("Vælg filtype! \nSkriv ad flere gange for at " + 
                             "tillade flere slags filtyper. Skriv !slut "  +
                             "for at bekræfte.");
         
+        //Loop der bliver ved med at spørge hvilke filtyper, der skal i listen
         while (counter == 0) {
             String tempFiltype = sc.next();           
             
@@ -35,19 +31,26 @@ public class FileChecker {
                 filtypeListe.add(tempFiltype);
             }
         }
+        
         System.out.println(filtypeListe);
+        //Returnere til sidst den færdige liste af filtyper
         return filtypeListe;
     
 }
     
-    public boolean checker2(ArrayList<String> filtypeListe, String teststring) {
+    //Metode der tjekker om en af de indtastede filtyper er godkendt
+    //De to argumenter er listen af godkendte filtyper og navnet på aflveringen
+    public boolean checker(ArrayList<String> filtypeListe, String aflevering) {
         boolean validFiltype = false;
         
+        //Løber listen igennem med hver filtype, og tjekker dem hver for sig
         for (String type : filtypeListe) {
+            //Indsætter en filtype fra listen og laver et RegEx-pattern
             newPattern = "\\." + type + "\\z";
             Pattern p = Pattern.compile(newPattern);
-            Matcher m = p.matcher(teststring);
+            Matcher m = p.matcher(aflevering);
             
+            //Tjekker om RegEx-mønstret passede, og sætter ValidFiltype = true, hvis det er
             if (m.find() || (validFiltype == true)) {
                 validFiltype = true;
             } else {
@@ -55,29 +58,26 @@ public class FileChecker {
             }
         }
         
+        //Returnere true, hvis en af filtyperne er godkendte
         return validFiltype;
     }
-    
-    /*
-    public void fileSelector() {
-        System.out.println("Vælg filtype:");
-        
-        String filtype = sc.next();
-        newPattern = "\\." + filtype + "\\z";
-        p = Pattern.compile(newPattern);
-        
-        System.out.println(newPattern);
-    }
-    
-    
-    
-    public boolean checker(String teststring){
-        Matcher m = p.matcher(teststring);
-        
-        return m.find();
+
+    //Get og set metoder
+    public String getNewPattern() {
+        return newPattern;
     }
 
-    */
+    public void setNewPattern(String newPattern) {
+        this.newPattern = newPattern;
+    }
+
+    public ArrayList<String> getFiltypeListe() {
+        return filtypeListe;
+    }
+
+    public void setFiltypeListe(ArrayList<String> filtypeListe) {
+        this.filtypeListe = filtypeListe;
+    }
     
     
 }
